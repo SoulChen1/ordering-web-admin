46fb26eaf93a39d518770c55653dc12808631f61
Jack <870964443@qq.com>
2021-04-22 01:42:02 +0800

    制作商品添加页

--- src/pages/product/ProductList.vue
+++ src/pages/product/ProductList.vue
@@ -8,17 +8,19 @@
           <a-row>
             <a-col :md="8" :sm="24">
               <a-form-item label="商品类别" :labelCol="{span: 5}" :wrapperCol="{span: 18, offset: 1}">
-                <a-input placeholder="请输入"></a-input>
+                <a-select allowClear="true" placeholder="请选择" v-model="form.category_id">
+                  <a-select-option v-for="item in categories" :key="item.id" :value="item.id">{{item.name}}</a-select-option>
+                </a-select>
               </a-form-item>
             </a-col>
             <a-col :md="8" :sm="24">
               <a-form-item label="商品名称" :labelCol="{span: 5}" :wrapperCol="{span: 18, offset: 1}">
-                <a-input placeholder="请输入"></a-input>
+                <a-input placeholder="请输入" v-model="form.name"></a-input>
               </a-form-item>
             </a-col>
             <a-col :md="8" :sm="24">
               <a-form-item label="状态" :labelCol="{span: 5}" :wrapperCol="{span: 18, offset: 1}">
-                <a-select placeholder="请选择">
+                <a-select placeholder="请选择" v-model="form.status">
                   <a-select-option value="0">已下架</a-select-option>
                   <a-select-option value="1">已上架</a-select-option>
                 </a-select>
@@ -27,8 +29,8 @@
           </a-row>
         </div>
         <span style="float: right; margin-top: 3px;">
-          <a-button type="primary">查询</a-button>
-          <a-button style="margin-left: 8px">重置</a-button>
+          <a-button type="primary" @click="selectedProd">查询</a-button>
+          <a-button style="margin-left: 8px" @click="clearForm">重置</a-button>
         </span>
       </a-form>
     </div>
@@ -49,18 +51,15 @@
           @change="onChange"
           @selectedRowChange="onSelectChange">
 
-        <div slot="description" slot-scope="{text}">
-          {{text}}
-        </div>
         <div slot="action" slot-scope="{text, prod}">
           <a style="margin-right: 8px">
             <a-icon type="plus"/>新增
           </a>
           <a style="margin-right: 8px">
-            <a-icon type="edit"/>新增
+            <a-icon type="edit"/>修改
           </a>
           <a @click="deletedProd(prod.id)">
-            <a-icon type="plus"/>删除
+            <a-icon type="delete"/>删除
           </a>
         </div>
         <template slot="statusTitle">
@@ -68,6 +67,48 @@
         </template>
       </standard-table>
     </div>
+
+    <!-- 新增商品对话框 -->
+    <a-modal v-model="visible" title="新增商品" on-ok="handleOk">
+      <template slot="footer">
+        <a-button key="back" @click="handleCencel">
+          取消
+        </a-button>
+        <a-button key="submit" type="primary" :loading="loading" @click="handleSubmit"></a-button>
+      </template>
+      <a-form-model :model="prod" label-col="{ span: 4 }" wrapper-col="{ span: 14 }">
+        <a-form-model-item label="商品名称">
+          <a-input v-model="prod.name"></a-input>
+        </a-form-model-item>
+        <a-form-model-item label="商品描述">
+          <a-input v-model="prod.description" type="textarea"></a-input>
+        </a-form-model-item>
+        <a-form-model-item label="商品价格">
+          <a-input v-model="prod.price"></a-input>
+        </a-form-model-item>
+        <a-form-model-item label="排序">
+          <a-input v-model="prod.sort"></a-input>
+        </a-form-model-item>
+        <a-form-model-item label="商品图片">
+          <a-input v-model="prod.img"></a-input>
+        </a-form-model-item>
+        <a-form-model-item label="状态">
+          <a-input v-model="prod.status"></a-input>
+        </a-form-model-item>
+        <a-form-model-item label="店铺">
+          <a-input v-model="prod.store_id"></a-input>
+        </a-form-model-item>
+        <a-form-model-item label="商品分类">
+          <a-input v-model="prod.category_id"></a-input>
+        </a-form-model-item>
+        <a-form-model-item label="库存">
+          <a-input v-model="prod.stock"></a-input>
+        </a-form-model-item>
+        <a-form-model-item label="是否推荐">
+          <a-input v-model="prod.is_recommend"></a-input>
+        </a-form-model-item>
+      </a-form-model>
+    </a-modal>
   </a-card>
 </template>
 
@@ -76,6 +117,8 @@ import StandardTable from "@/components/table/StandardTable"
 import {getProductList} from "@/services/product"
 import {getCategoryOption} from "@/services/category"
 
+const dataSource = [];
+let categories = {};
 const columns = [
   {
     title: 'ID',
@@ -88,7 +131,15 @@ const columns = [
   {
     title: '商品描述',
     dataIndex: 'description',
-    scopedSlots: { customRender: 'description'}
+
+    ellipsis: true
+  },
+  {
+    title: '商品分类',
+    dataIndex: 'category_id',
+    customRender: (category) => {
+      return categories[category].name ? categories[category].name : category;
+    }
   },
   {
     title: '价格',
@@ -107,16 +158,11 @@ const columns = [
     }
   },
   {
-    title: '商品分类',
-    dataIndex: 'category_id'
-  },
-  {
     title: '操作',
     scopedSlots: { customRender: 'action'}
   }
 ];
 
-const dataSource = [];
 
 export default {
   name: "ProductList",
@@ -129,7 +175,8 @@ export default {
       selectedRows: [],
       pagination: {},
       loading: false,
-      categories: []
+      categories: categories,
+      form: {}
     }
   },
   mounted() {
@@ -138,6 +185,7 @@ export default {
   methods: {
     init() {
       this.getDataSource()
+      this.getCategories()
     },
     getDataSource(params = {}){
       this.loading = true;
@@ -151,8 +199,15 @@ export default {
     },
     getCategories() {
       getCategoryOption().then(res => {
-
+        categories = res.data;
+        this.categories = res.data;
       });
+    },
+    selectedProd() {
+      this.getDataSource(this.form);
+    },
+    clearForm() {
+      this.form = {};
     },
     addProd() {
       console.log('添加商品');
